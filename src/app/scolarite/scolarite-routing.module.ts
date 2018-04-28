b874bb5acbbc6d3c2125e90ee190de26be269603
med0001 <messaoudimed9@gmail.com>
2018-04-29 00:01:37 +0100

    v1
    modification apporter a section scolarite (view)

--- src/app/scolarite/scolarite-routing.module.ts
+++ src/app/scolarite/scolarite-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScolariteComponent } from './scolarite.component';
+import { HomeComponent } from './home/home.component';
+import { AttestationComponent } from './attestation/attestation.component';
 
 const routes: Routes = [
   {
     path:'esp_scolarite',
     component:ScolariteComponent,
-    //children: [{ path: '2', component: HomeComponent }],
+    children: [{ path: 'home', component: HomeComponent }],
+  },
+  {
+    path:'esp_scolarite',
+    component:ScolariteComponent,
+    children: [{ path: 'all', component: AttestationComponent }],
   }
 ];
 
--- src/app/scolarite/scolarite.module.ts
+++ src/app/scolarite/scolarite.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ScolariteRoutingModule } from './scolarite-routing.module';
 import { ScolariteComponent } from './scolarite.component';
 import { MenuScolariteComponent } from './menu-scolarite/menu-scolarite.component';
+import { AttestationComponent } from './attestation/attestation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ScolariteRoutingModule
   ],
-  declarations: [ScolariteComponent, MenuScolariteComponent]
+  declarations: [ScolariteComponent, MenuScolariteComponent, AttestationComponent, HomeComponent]
 })
 export class ScolariteModule { }
